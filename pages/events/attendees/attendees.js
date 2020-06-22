e3779a3599b27be1e8a1c2789e3bb7fccaf9355c
Agnes Selvita <a.selvita@gmail.com>
2020-06-22 11:15:36 +0800

    listview  true false controller

--- pages/events/attendees/attendees.js
+++ pages/events/attendees/attendees.js
@@ -3,12 +3,13 @@ const BC = require('../../../libs/bc');
 Page({
   data: {
     selectedId: null,
-    listView: true,
+    listView: false,
   },
 
   onLoad: function (options) {
     wx.hideShareMenu()
-    this.setData({ attendees: BC.lastPage().data.event.attendees, event: BC.lastPage().data.event})    
+    this.setData({ attendees: BC.lastPage().data.event.attendees, event: BC.lastPage().data.event, userInfo: BC.lastPage().data.userInfo, creator: BC.lastPage().data.creator})
+    if (this.data.creator.user.id == this.data.userInfo.id) {this.setData({listView: true})}
   },
 
   tapUser(e) {
