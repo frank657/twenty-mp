ab87e0afe8104b7e66ef711dd924dd3a21849e51
Frank Zhu <qf.zhu.frank@gmail.com>
2021-01-22 14:24:12 +0800

    version change

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ App({
       none: '',
       background: '#FFFDF8'
     },
-    version: '1.3.5',
+    version: '1.3.6',
     versionChecked: false,
     lang: 'en', // cn or en
     userInfo: null,
